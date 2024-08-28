aff21dfc4fbf26eca6619800ba27bea36fcb9329
muendokennedy <kennedymunyao999@gmai.com>
2024-08-28 10:56:38 +0300

    Implemented the sum of digits using tail recursion

--- lib/recursion/sum_digits.ex
+++ lib/recursion/sum_digits.ex
@@ -3,7 +3,15 @@ defmodule Elixirtut.Recursion.SumDigits do
   def sum_upto(0), do: 0
 
   def sum_upto(num) do
-    num + sum_upto(num - 1)
+    sum_upto_tail_rec(num)
+  end
+
+  def sum_upto_tail_rec(num, acc \\ 0)
+
+  def sum_upto_tail_rec(0, acc), do: acc
+
+  def sum_upto_tail_rec(num, acc) do
+    sum_upto_tail_rec(num - 1, acc + num)
   end
 
 end
