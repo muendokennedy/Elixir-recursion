d6f74fe396e1025a1ae1480b01f5afc8a3baf9a5
muendokennedy <kennedymunyao999@gmai.com>
2024-08-28 11:15:48 +0300

    Implemented factorial with tail recursion

--- lib/recursion/factorial.ex
+++ lib/recursion/factorial.ex
@@ -4,4 +4,10 @@ defmodule Elixirtut.Recursion.Factorial do
   def factorial(0), do: 1
 
   def factorial(num), do: num * factorial(num - 1)
+
+  def factorial_tail_rec(num, acc \\ 1)
+
+  def factorial_tail_rec(1, acc), do: acc
+
+  def factorial_tail_rec(num, acc), do: factorial_tail_rec(num - 1, acc * num)
 end
