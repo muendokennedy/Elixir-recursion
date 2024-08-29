48a763a448af84f5df3f368e191e612e43f63ec2
muendokennedy <kennedymunyao999@gmai.com>
2024-08-29 16:10:18 +0300

    Learnt how to documentations and type specifications in elixir

--- lib/lists/lists.ex
+++ lib/lists/lists.ex
@@ -1,8 +1,23 @@
 defmodule Elixirtut.Lists do
+  @moduledoc """
+
+  Function summary:
+  Sum of numbers in a list using head and tail recursion
+  """
+
+  @doc """
+  Return the sum of numbers in a list using head recursion
+  """
+  @spec sum(list(number())) :: number()
+
+  def sum(nums), do: sum_tail_rec(nums)
+
+  @spec sum_simple(list(number())) :: number()
 
   def sum_simple([]), do: 0
-
   def sum_simple([h | t]), do: h + sum_simple(t)
+
+  @spec sum_tail_rec(list(number()), integer()) :: number()
 
   def sum_tail_rec(nums, acc \\ 0)
   def sum_tail_rec([], acc), do: acc
