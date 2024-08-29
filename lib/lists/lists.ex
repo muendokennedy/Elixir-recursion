91bd902dd38694e189096843dbf608a700a5dbb3
muendokennedy <kennedymunyao999@gmai.com>
2024-08-29 15:44:45 +0300

    Implemented the sum of numbers in a list using tail recursion

--- lib/lists/lists.ex
+++ lib/lists/lists.ex
@@ -1,6 +1,11 @@
 defmodule Elixirtut.Lists do
+
   def sum_simple([]), do: 0
 
   def sum_simple([h | t]), do: h + sum_simple(t)
+
+  def sum_tail_rec(nums, acc \\ 0)
+  def sum_tail_rec([], acc), do: acc
+  def sum_tail_rec([h | t], acc), do: sum_tail_rec(t, acc + h)
 
 end
