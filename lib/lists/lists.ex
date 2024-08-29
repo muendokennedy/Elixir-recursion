51b159f818d84bcabd02e7969cb5d2620f480d92
muendokennedy <kennedymunyao999@gmai.com>
2024-08-29 18:27:54 +0300

    Implemented the logic to map through a list using an anonymous callback function

--- lib/lists/lists.ex
+++ lib/lists/lists.ex
@@ -23,4 +23,24 @@ defmodule Elixirtut.Lists do
   def sum_tail_rec([], acc), do: acc
   def sum_tail_rec([h | t], acc), do: sum_tail_rec(t, acc + h)
 
+  # --------------------------------- Reverse -----------------------------
+
+  @doc """
+  Return the reverse of the elements in a list.
+  """
+  @spec reverse([any()], [any()]) :: [any()]
+
+  def reverse(elements, acc \\ [])
+  def reverse([], acc), do: acc
+  def reverse([h | t], acc), do: reverse(t, [h | acc])
+
+  # --------------------------  Map ---------------------------------------
+  @doc """
+  Return a new list that has it's elements altered through a callback function
+  """
+  @spec map([(any())], (any() -> any()), [any()]) :: [any()]
+  def map(elements, func, acc \\ [])
+  def map([], _, acc), do: reverse(acc)
+  def map([h | t], func, acc), do: map(t, func, [func.(h) | acc])
+
 end
