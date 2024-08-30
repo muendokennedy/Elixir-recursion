2268b87cc7e012c95d3b105c396aa64cbafe8d06
muendokennedy <kennedymunyao999@gmai.com>
2024-08-30 11:43:30 +0300

    Inspected a number of functionalities using the elixir inbuilt functions in the Enum module

--- lib/structs/seven_wonders.ex
+++ lib/structs/seven_wonders.ex
@@ -15,7 +15,10 @@ defmodule Elixirtut.Structs.SevenWonders do
     [
       %SevenWonders{name: "The Great Wall of China", country: "China"},
       %SevenWonders{name: "The Great Pyramids", country: "Egypt"},
-      %SevenWonders{name: "The Burg Khalifa", country: "Dubai"}
+      %SevenWonders{name: "The Burg Khalifa", country: "Dubai"},
+      %SevenWonders{name: "The cow", country: "India"},
+      %SevenWonders{name: "Colossum", country: "Italy"},
+      %SevenWonders{name: "Christ The Redeemer", country: "Brazil"}
     ]
   end
 
@@ -29,6 +32,28 @@ defmodule Elixirtut.Structs.SevenWonders do
   @spec filter_by_country([t()], String.t()) :: [t()]
   def filter_by_country(wonders, country) do
     wonders |> Enum.filter(fn %{country: country_name} -> country_name == country end)
+  end
+
+  @spec in_countries_starting_with_i([t()]) :: [t()]
+  def in_countries_starting_with_i(wonders) do
+    wonders |> Enum.filter(fn %{country: country} -> String.starts_with?(country, "I") end)
+  end
+
+  def sort_by_country_length(wonders) do
+    wonders |> Enum.sort(fn x, y -> String.length(x.country) < String.length(y.country) end)
+  end
+
+  def name_country_list(wonders) do
+    wonders |> Enum.reduce([], fn wonder, acc -> [[wonder.name, wonder.country] | acc] end)
+  end
+
+  def country_name_keyword_list(wonders) do
+    wonders |> Enum.reduce([], fn wonder, acc -> [{String.to_atom(wonder.name), wonder.country} | acc] end)
+  end
+
+  # ---------------------------- For ------------------------------
+  def all_names(wonders) do
+    for %{name: name} <- wonders, do: name
   end
 
 end
