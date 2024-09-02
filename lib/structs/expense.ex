9ea42fd5f2273145b869a1ed63cec987c970940f
muendokennedy <kennedymunyao999@gmai.com>
2024-09-02 14:41:08 +0300

    Implemented the functionality of find find the total number of expenses

---
+++ lib/structs/expense.ex
@@ -0,0 +1,34 @@
+defmodule Elixirtut.Structs.Expense do
+
+  alias Elixirtut.Structs.Expense
+
+  defstruct(
+    title: "",
+    date: nil,
+    amount: 0,
+    store: ""
+  )
+
+  @type t :: %Expense{
+    title: String.t(),
+    date: Date.t(),
+    amount: number(),
+    store: String.t()
+  }
+
+  @spec sample :: [t()]
+  def sample do
+    [
+      %Expense{title: "Grocery", date: ~D[2024-09-01], amount: 20.00, store: "Metro"},
+      %Expense{title: "Mobile", date: ~D[2023-09-04], amount: 25.00, store: "Nike"},
+      %Expense{title: "Jacket", date: ~D[2023-09-04], amount: 26.00, store: "Marks"},
+      %Expense{title: "Jordan", date: ~D[2023-09-04], amount: 21.00, store: "Bell"},
+      %Expense{title: "Cell Phone", date: ~D[2023-09-04], amount: 18.00, store: "Naboka"}
+    ]
+  end
+
+  @spec total([t]) :: number()
+  def total(expenses) do
+    expenses |> Enum.reduce(0, fn  expense, acc -> expense.amount + acc  end)
+  end
+end
